e0b2a8569855f9edbf1549726558ee4a344004e8
Egor Kislitsyn <egor@kislitsyn.com>
2025-09-01 22:30:51 +0400

    seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,9 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     CinderEmbeddedSort.Repo.insert!(%CinderEmbeddedSort.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+for i <- 1..100 do
+  Ash.Seed.seed!(CinderEmbeddedSort.MyDomain.MyResource, %{
+    name: "item_#{i}",
+    settings: %{
+      a: "value_a_#{i}",
+      b: i
+    }
+  })
+end
